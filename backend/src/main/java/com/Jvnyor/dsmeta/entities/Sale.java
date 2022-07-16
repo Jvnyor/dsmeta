755da6cd33cba847552771f96e6dc7af1f97b3d0
Jvnyor <josias100junior@gmail.com>
2022-07-16 17:30:36 -0300

    Database

---
+++ backend/src/main/java/com/Jvnyor/dsmeta/entities/Sale.java
@@ -0,0 +1,33 @@
+package com.Jvnyor.dsmeta.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_sales")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Sale {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String sellerName;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+}
