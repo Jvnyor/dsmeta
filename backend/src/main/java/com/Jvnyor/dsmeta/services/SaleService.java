d038f9e111f39a992febeb02bb1306bac0cfadf1
Jvnyor <josias100junior@gmail.com>
2022-07-16 19:02:39 -0300

    Date select

--- backend/src/main/java/com/Jvnyor/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/Jvnyor/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.Jvnyor.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.Jvnyor.dsmeta.entities.Sale;
@@ -13,8 +17,14 @@ public class SaleService {
 
 	@Autowired
 	private SaleRepository repository;
-	
-	public List<Sale> findSales() {
-		return repository.findAll();
+
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+		LocalDate min = !minDate.isBlank() ? LocalDate.parse(minDate) : today.minusDays(365);
+		LocalDate max = !maxDate.isBlank() ? LocalDate.parse(maxDate) : today;
+
+		return repository.findSales(min, max, pageable);
 	}
 }
