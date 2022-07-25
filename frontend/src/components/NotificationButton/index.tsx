98a3a294ea6b270b40f50e5f4d57f11419951b66
Josias Junior <74216703+Jvnyor@users.noreply.github.com>
2022-07-25 11:08:31 -0300

    Bug component NotificationButton fix

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -17,11 +17,9 @@ function handleClick( id : number ) {
 
 function NotificationButton( {saleId} : Props ) {
     return (
-        <>
-            <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
-                <img src={icon} alt="Notificar" />
-            </div>
-        </>
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+            <img src={icon} alt="Notificar" />
+        </div>
     )
 }
 
